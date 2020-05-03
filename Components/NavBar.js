ddaf880199e308061249f8ba0ebf0e1a56bf6bec
Hossein <mohammadhossein211@gmail.com>
2020-05-03 22:13:43 +0430

    Initial commit

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
-
+// import altrabo from './images/altrabo.png';
 class NavBar extends Component {
   constructor(props) {
     super(props);
@@ -9,49 +9,58 @@ class NavBar extends Component {
   render() {
     return (
       <div className='NavBar'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <div className='container'>
-            <Link href='/'>
-              <a className='navbar-brand'>My cool blog</a>
-            </Link>
-            <button
-              className='navbar-toggler'
-              type='button'
-              data-toggle='collapse'
-              data-target='#navbarSupportedContent'
-              aria-controls='navbarSupportedContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-            >
-              <span className='navbar-toggler-icon'></span>
-            </button>
-
-            <div
-              className='collapse navbar-collapse'
-              id='navbarSupportedContent'
-            >
-              <ul className='navbar-nav mr-auto'>
-                <li className='nav-item active'>
-                  <Link href='/'>
-                    <a className='nav-link'>
-                      Home <span className='sr-only'>(current)</span>
-                    </a>
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link href='/signin'>
-                    <a className='nav-link'>Sign In</a>
-                  </Link>
-                </li>{' '}
-                <li className='nav-item'>
-                  <Link href='/signup'>
-                    <a className='nav-link'>Sign Up</a>
-                  </Link>
-                </li>
-              </ul>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg bg-white'>
+            <div className='container'>
+              <button
+                className='navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+              >
+                <span className='navbar-toggler-icon'>h</span>
+              </button>
+              <div
+                className='collapse navbar-collapse'
+                id='navbarSupportedContent'
+              >
+                <ul className='navbar-nav ml-auto'>
+                  <li className='nav-item active'>
+                    <Link href='/'>
+                      <img src='images/altrabo.png' alt='' />
+                    </Link>
+                  </li>
+                  <li className='nav-item active'>
+                    <Link href='/'>
+                      <a className='nav-link'>بلیت هواپیما</a>
+                    </Link>
+                  </li>
+                  <li className='nav-item active'>
+                    <Link href='/'>
+                      <a className='nav-link'>بلیت اتوبوس</a>
+                    </Link>
+                  </li>
+                  <li className='nav-item active'>
+                    <Link href='/'>
+                      <a className='nav-link'>رزرو هتل</a>
+                    </Link>
+                  </li>
+                  <li className='nav-item active'>
+                    <Link href='/'>
+                      <a className='nav-link'>
+                        <i class='fas fa-user ml-2' />
+                        ورود/ عضویت
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
+        </div>
       </div>
     );
   }
